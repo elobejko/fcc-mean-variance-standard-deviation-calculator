2efef6b79f42020cfe4e0d01d39391b27ba0f444
Ewa Łobejko <lobejkoewa@gmail.com>
2020-11-09 17:29:34 +0000

    Return calculations dictionary

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,17 @@
 import numpy as np
 
 def calculate(list):
+  if len(list) != 9:
+    return "List must contain nine numbers."
+ 
+  lst = np.reshape(np.array(list), (-1, 3))
+  calculations = {}
+ 
+  calculations['mean'] = [lst.mean(axis = 0).tolist(), lst.mean(axis = 1).tolist(), lst.mean()]
+  calculations['variance'] = [lst.var(axis = 0).tolist(), lst.var(axis = 1).tolist(), lst.var()]
+  calculations['standard deviation'] = [lst.std(axis = 0).tolist(), lst.std(axis = 1).tolist(), lst.std()]
+  calculations['max'] = [lst.max(axis = 0).tolist(), lst.max(axis = 1).tolist(), lst.max()]
+  calculations['min'] = [lst.min(axis = 0).tolist(), lst.min(axis = 1).tolist(), lst.min()]
+  calculations['sum'] = [lst.sum(axis = 0).tolist(), lst.sum(axis = 1).tolist(), lst.sum()]
 
-
-
-
-    return calculations
+  return calculations
